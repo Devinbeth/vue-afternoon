692e5548437b583d31e1dfee81c8893f63e26af1
Devin Lethbridge <devinbeth@gmail.com>
2018-03-29 16:49:19 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Location/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Location from './components/Location.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Location
   }
 }
 </script>
---
+++ src/components/Location.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="hello">
+    <nav class='navbar' >
+   <input placeholder="Location"
+    v-on:keydown.enter="updateLocation"/>
+    </nav>
+    <p>{{ location }}</p>
+    <iframe
+      width="600"
+      height="450"
+      frameborder="0" style="border:0"
+      :src="formattedLocation" allowfullscreen>
+    </iframe>
+    <div>
+      <h2>Shapes</h2>
+      <div class="shape" :class="{ diff: clicked }" @click="changeClick"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Location",
+  props: {},
+  data() {
+    return {
+      location: "",
+      formattedLocation: "",
+      click: false
+    };
+  },
+  methods: {
+    updateLocation: function(event) {
+      this.location = event.target.value;
+      this.createLocation()
+    },
+    createLocation: function() {
+      let newStr = this.location.replace(/ /g, '');
+      this.formattedLocation = `https://www.google.com/maps/embed/v1/place?key=AIzaSyDPOxgpLyUmh5ylWGSvrWLloRxmzqPUvv8&q=${newStr}`;
+    },
+    changeClick: function() {
+      this.changeClick = !this.changeClick;
+    }
+  }
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.navbar {
+  background: #42b983;
+  width: 100%;
+  height: 50px;
+  justify-content: center;
+  align-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+input {
+  padding: 10px;
+  margin-top: 5px;
+}
+.shape {
+  height: 50px;
+  width: 50px;
+  background: blue;
+}
+.diff {
+  background: red;
+}
+</style>
